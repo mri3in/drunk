7de29bcafcc6863f07d3aa44f52cfe7a88f0a8e5
mri3in <i3in.3vil@gmail.com>
2020-10-04 00:28:15 +0700

    update url

--- drunk/urls.py
+++ drunk/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('drunk/', include("shots.urls"))
+    path('shots/', include("shots.urls"))
 ]
