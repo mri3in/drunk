63e7c2b637e91b775a21d3eabe481184f02eb365
mri3in <i3in.3vil@gmail.com>
2020-10-05 13:52:24 +0700

    create db load 1st event to index with its participants

--- shots/models.py
+++ shots/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Participant (models.Model):
+    name = models.CharField(max_length=64)
+    nickname = models.CharField(max_length=64)
+    type = models.BooleanField(null=True)
+
+    def __str__ (self):
+        return f"{self.name} - {self.nickname}"
+
+class Event (models.Model):
+    name = models.CharField(max_length=64)
+    date = models.DateField(auto_now=False, auto_now_add=True, editable=False)
+
+    def __str__ (self):
+        return f"{self.name} - {self.date}"
+
+
+class Round (models.Model):
+    datetime = models.DateTimeField(auto_now=False, auto_now_add=True, editable=False)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="rounds")
+    total = models.IntegerField()
+    note = models.CharField(max_length=128)
+    payee = models.ForeignKey(Participant, on_delete=models.CASCADE, related_name="payees")
+    participant = models.ManyToManyField(Participant, through='RoundParticipant')
+
+    def __str__ (self):
+        return f"{self.event} - {self.datetime}"
+
+class RoundParticipant (models.Model):
+    round = models.ForeignKey(Round, on_delete=models.CASCADE)
+    participant = models.ForeignKey(Participant, on_delete=models.CASCADE, related_name="roundParticipant")
+    quantity = models.IntegerField()
+
+    def __str__ (self):
+        return f"{self.round} - {self.participant}: {self.quantity}"
--- shots/views.py
+++ shots/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'shots/index.html')
+    event = Event.objects.order_by('date')[0]
+    rounds = event.rounds.all()
+    participants = []
+    for round in rounds:
+        rpList = RoundParticipant.objects.filter(round__id=round.id)
+        print(round)
+        print(f"rpList: {rpList}")
+        for rp in rpList:
+            print(rp)
+            if rp.participant not in participants:
+                participants.append(rp.participant)
+
+    print(participants)
+    print(event)
+    print(rounds)
+    return render(request, 'shots/index.html', {
+        "event": event,
+        "participants": participants
+    })
