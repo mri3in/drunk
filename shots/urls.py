f2edc28d96fadd1318a4b6dbbe077479893e2ab5
mri3in <i3in.3vil@gmail.com>
2020-10-06 18:09:08 +0700

    submit the form with participant and quantity

--- shots/urls.py
+++ shots/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = "shots"
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path("addRound", views.addRound, name="addRound")
+    # path("test", views.test, name="test")
 ]
--- shots/views.py
+++ shots/views.py
@@ -1,24 +1,47 @@
 from django.shortcuts import render
 from .models import *
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    event = Event.objects.order_by('date')[0]
-    rounds = event.rounds.all()
+    lastEvent = Event.objects.order_by('-date')[0]
+    events = Event.objects.order_by('-date')
+    rounds = lastEvent.rounds.order_by('-datetime')
+    lastRound = rounds[0]
     participants = []
     for round in rounds:
-        rpList = RoundParticipant.objects.filter(round__id=round.id)
-        print(round)
+        rpList = RoundParticipant.objects.filter(round=round.id)
+        print(f"round: {round}")
         print(f"rpList: {rpList}")
         for rp in rpList:
-            print(rp)
+            print(f"roundparticipant : {rp}")
             if rp.participant not in participants:
                 participants.append(rp.participant)
-
-    print(participants)
-    print(event)
-    print(rounds)
+    print("---------")
+    print(f"participants: {participants}")
+    print(f"lastEvent: {lastEvent}")
+    print(f"rounds: {rounds}")
     return render(request, 'shots/index.html', {
-        "event": event,
-        "participants": participants
+        "lastEvent": lastEvent,
+        "participants": participants,
+        "events": events,
+        "rounds": rounds,
+        "lastRound": lastRound
     })
+
+
+def addRound(request):
+    if request.method == "POST":
+        participants = request.POST.getlist('participant')
+        quantities = [request.POST['quantity-{}'.format(p)] for p in participants]
+        list = zip(participants, quantities)
+        # print(f"participants: {participants}")
+        # print(f"quantities: {quantities}")
+        # return HttpResponseRedirect(reverse("shots:test", args=(participants, quantities,)))
+        # return HttpResponseRedirect(reverse("shots:test"))
+        return render(request, 'shots/test.html', {
+            "participants":participants,
+            "quantities":quantities,
+            "list": list
+        })
