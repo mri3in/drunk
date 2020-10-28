89b6a67addbfcffd021c757319347ea6cd4da3a1
mri3in <i3in.3vil@gmail.com>
2020-10-29 02:35:36 +0700

    update landing page

--- drunk/urls.py
+++ drunk/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+from django.conf import settings
+
+
+def staticView(request):
+    with open(str(settings.BASE_DIR)+"/index.html") as fp:
+        return HttpResponse(fp.read())
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shots/', include("shots.urls"))
+    path('shots/', include("shots.urls")),
+    path('', staticView)
 ]
